e3c47483b2156f4319e8c3f60970447381651dee
Rozalyn <96503223+ZolaraCodes@users.noreply.github.com>
2022-11-08 16:30:58 -0500

    change spotlight background

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const spotlightEl = document.querySelector("#spotlight");
 function handleMouseMove(event) {
     const { clientX, clientY } = event;
     
-    spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 10px, #000000ee 350px)`;
+    spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px,  #ff375a 10px, #000000ee 650px)`;
 }
 
 document.addEventListener("mousemove", handleMouseMove)
