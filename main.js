1e126778963341978e0c8b290b3db7df92b124f7
Rozalyn <96503223+ZolaraCodes@users.noreply.github.com>
2022-11-08 18:37:21 -0500

    change spotlight and size

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const spotlightEl = document.querySelector("#spotlight");
 function handleMouseMove(event) {
     const { clientX, clientY } = event;
     
-    spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px,  #ff375a 10px, #000000ee 650px)`;
+    spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00008b 10%, 20px , #000000ee 350px)`;
 }
 
 document.addEventListener("mousemove", handleMouseMove)
+
